c9552bcda0c0e0805686dab083001aa9ef367223
Farouk Goaudria <gouadriaafar@gmail.com>
2022-08-03 12:53:07 +0100

    Create addTicket_Controller

--- controller/ticket.js
+++ controller/ticket.js
@@ -1,7 +1,25 @@
 const Ticket = require('../model/ticket');
+const User = require('../model/user');
 
 const TicketController = {
-
+    
+        AddTicket:async(req,res)=>{
+                try {
+                    const _id=req.body.id
+                    const {magasin,products,dateAchat,prixTotal}=req.body
+                    console.log({userId:_id,magasin:magasin,products:products,proxTotal:prixTotal,dateAchat:dateAchat})
+                    const user =  await User.findById(_id);
+                    if(!user){
+                        return res.status(404).json({success:'false',message:'USer Not Found !'})
+                    }else
+                        {
+                            return res.status(200).json({success:'true',data:{nom:user.nom,prenom:user.prenom}})
+                        }
+                    
+                } catch (error) {
+                    res.status(500).json({success:'false',message:error})
+                }
+        }
 }
 
 module.exports = TicketController
--- routes/ticket.js
+++ routes/ticket.js
@@ -2,7 +2,8 @@ const express = require("express");
 const RouterTicket = express();
 
 const bodyParser = require("body-parser");
-
+const TicketController = require("../controller/ticket");
+        RouterTicket.post('/addTicket',TicketController.AddTicket);
 
 
 module.exports = RouterTicket;
