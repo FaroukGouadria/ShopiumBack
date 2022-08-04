856862868be3dbe7dc6a4aa984dc16890e8ffc94
Farouk Goaudria <gouadriaafar@gmail.com>
2022-08-04 13:43:24 +0100

    .

--- routes/ticket.js
+++ routes/ticket.js
@@ -4,6 +4,6 @@ const RouterTicket = express();
 const bodyParser = require("body-parser");
 const TicketController = require("../controller/ticket");
         RouterTicket.post('/addTicket',TicketController.AddTicket);
-router.post("/getUserInfo",TicketController.getUserTicket );
+RouterTicket.post("/getUserInfo",TicketController.getUserTicket );
 
 module.exports = RouterTicket;
