ca0c01c941df398edacb5e688394f0893e04b92c
Farouk Goaudria <gouadriaafar@gmail.com>
2022-08-17 12:32:38 +0100

    try 1 after add ticket and verify if ticket had product offer

--- controller/ticket.js
+++ controller/ticket.js
@@ -8,6 +8,9 @@ const TicketController = {
     
         AddTicket:async(req,res)=>{
                 try {
+                    let productTicket;
+                    let productTicketDetail;
+                    let montantARembourser ;
                     const _id=req.body.id
                     const recu=req.body.recu
                     console.log({id:_id,recu:recu.products})
@@ -27,6 +30,54 @@ const TicketController = {
 
                            await ticket.save();
                            console.log({ticket})
+//::::::::::::::::::::::::::::::: block verification if product in ticket had offer or not ::::::::::::::::::::::::::::::::::::::: 
+                    const productOfTicket = ticket.map((item)=>item.Product)
+                    console.log({productOfTicket})
+                    const ticketProduct   = productOfTicket.map((item,i)=>{
+                        productTicket= item.map((i)=>i.pname)  
+                    });
+                    console.log({productTicket})
+                    ////a verifier offer toul 
+                    const ProductOffer = await OfferModel.find();
+                    const nameProduct =ProductOffer.map(item=>item.productName);
+                    console.log({nameProduct})
+                    const intersection = productTicket.filter(element=>nameProduct.includes(element)).toString();
+                    if(!intersection){
+                        return res.status(404).json("pas de offer dans votre ticket ")
+                        
+                    }else{
+
+                        console.log({intersection})
+                       const checkProduct = await OfferModel.findOne({productName:intersection});
+                       console.log({checkProduct: checkProduct})
+                       const detailTicket = productOfTicket.map((item,i)=>{
+                       productTicketDetail =item.filter((element)=>element.pname===intersection);
+                       });
+                       if(productTicketDetail){
+                           console.log({productTicketDetail})
+                           productTicketDetail.map((item)=>{
+                               montantARembourser = (checkProduct.percentage/100)*item.pquantity*item.pupri;
+                           })
+                           console.log(typeof(montantARembourser))
+                           const userbeforeUpdate= await User.findById(idUser);
+                           console.log(userbeforeUpdate.cagnotte);
+                           const user = await User.findByIdAndUpdate({
+                               _id:_id
+                           },{
+                               cagnotte:userbeforeUpdate.cagnotte + montantARembourser,
+                               $push:{
+                                   historique:{
+                                       offerId:checkProduct._id,
+                                       montant:montantARembourser
+                                   }
+                               }
+       
+                           }).exec();
+                           return res.status(200).json({productOfTicket,nameProduct,productTicket,intersection,offer:{condition:checkProduct.condition,quantite:checkProduct.quantity,percentage:checkProduct.percentage},productTicketDetail,montantARembourser,user});
+                       }else{
+                           return res.status(404).json({message:"aucun offer dans votre ticket"})
+                       }
+                    }
                            return await res.status(200).json({ticket,message:"merci de scanner Votre ticket , nous vous répondrons dans les  48 heures au maximum"});
                         }
                     
