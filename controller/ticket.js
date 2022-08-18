0a36ba9adaff00e4616ecfdbc761682991de80ee
Farouk Goaudria <gouadriaafar@gmail.com>
2022-08-18 17:09:34 +0100

    push heroku server

--- controller/ticket.js
+++ controller/ticket.js
@@ -79,9 +79,7 @@ const TicketController = {
                                 console.log("error")
                             }
                         });  
-                        return res.status(200).json({ticket,message:` vous avez ${taille} offres dans votre ticket. 
-                            \n\n
-                             produits: ${intersection.toString()}`});
+                        return res.status(200).json({ticket,message:` vous avez ${taille} offres dans votre ticket.\nproduits: ${intersection.toString()}`});
                     }}
                         //    return await res.status(200).json({ticket,message:"merci de scanner Votre ticket , nous vous répondrons dans les  48 heures au maximum"});
                 } catch (error) {
