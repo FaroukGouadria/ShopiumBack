f762248813e2f4f31b9bb18520269f78e6787e6c
Farouk Goaudria <gouadriaafar@gmail.com>
2022-08-22 11:46:56 +0100

    add route

--- routes/ticket.js
+++ routes/ticket.js
@@ -8,5 +8,6 @@ RouterTicket.post("/getUserInfo",TicketController.getUserTicket );
 RouterTicket.post('/getTicketByUser',TicketController.getTicketByUser);
 RouterTicket.post('/sommeAchat',TicketController.sommeAchat);
 RouterTicket.post("/getalltickets", TicketController.getAllTicketByUser);
+RouterTicket.post("/changeticketstatus", TicketController.changeStatusTicket);
 
 module.exports = RouterTicket;
